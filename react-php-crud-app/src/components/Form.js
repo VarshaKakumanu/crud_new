ddccbafb99f8145fb74b7803741299d292bbab51
varsha <varshakakumanu@gmail.com>
2023-01-12 06:26:32 +0530

    yes

---
+++ react-php-crud-app/src/components/Form.js
@@ -0,0 +1,48 @@
+import { useState, useContext } from "react";
+import { AppContext } from "../Context";
+const Form = () => {
+  const { insertUser } = useContext(AppContext);
+  const [newUser, setNewUser] = useState({});
+
+  // Storing the Insert User Form Data.
+  const addNewUser = (e, field) => {
+    setNewUser({
+      ...newUser,
+      [field]: e.target.value,
+    });
+  };
+
+  // Inserting a new user into the Database.
+  const submitUser = (e) => {
+    e.preventDefault();
+    insertUser(newUser);
+    e.target.reset();
+  };
+
+  return (
+    <form className="insertForm" onSubmit={submitUser}>
+      <h2>Insert Project</h2>
+      <label htmlFor="_name">Project Name</label>
+      <input
+        type="text"
+        id="_name"
+        onChange={(e) => addNewUser(e, "user_name")}
+        placeholder="Enter name"
+        autoComplete="off"
+        required
+      />
+      <label htmlFor="_email">Task</label>
+      <input
+        type="text"
+        id="_email"
+        onChange={(e) => addNewUser(e, "user_email")}
+        placeholder="Enter task"
+        autoComplete="off"
+        required
+      />
+      <input type="submit" value="Insert" />
+    </form>
+  );
+};
+
+export default Form;
